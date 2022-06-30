5403bf7f4078ea8212f92a28a97fcb8f5f48cf52
Tungduong2001 <tungduong.kaka64@gmail.com>
2022-06-30 11:36:56 +0700

    Detail Product

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -1,14 +1,41 @@
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from 'next'
 import { useRouter } from 'next/router'
 import React from 'react'
 
-type Props = {}
+type ProductProps = {
+    product: any
+}
 
-const ProductsDetail = (props: Props) => {
-    const router = useRouter();
-    const id = router.query.id
+const ProductsDetail = ({ product }: ProductProps) => {
+    if (!product) return null
     return (
-        <div>ProductsDetail: {id}</div>
+        <div>ProductsDetail: {product.name} </div>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    const data = await (await fetch(`http://localhost:3001/products`)).json()
+    const paths = data.map((item: any) => {
+        return { params: { id: item.id } }
+    })
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context: GetStaticPropsContext) => {
+    const data = await (await fetch(`http://localhost:3001/products/${context.params?.id}`)).json()
+    if (!data) {
+        return {
+            notFound: true
+        }
+    }
+    return {
+        props: {
+            product: data
+        }
+    }
 }
 
 export default ProductsDetail
--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,11 +1,34 @@
+import { GetStaticProps, GetStaticPropsContext } from 'next'
+import Link from 'next/link';
 import React from 'react'
 
-type Props = {}
+type ProductProps = {
+    products: any[];
+}
 
-const ProductsPage = (props: Props) => {
+const ProductsPage = ({ products }: ProductProps) => {
+    if (!products) return null;
     return (
-        <div>ProductsPage</div>
+        <div>
+            {products.map((item) =>
+                <div key={item.id}><Link href={`/products/${item.id}`}>{item.name}</Link></div>
+            )}
+        </div>
     )
+}
+
+export const getStaticProps: GetStaticProps<ProductProps> = async (context: GetStaticPropsContext) => {
+    const data = await (await fetch(`http://localhost:3001/products`)).json()
+    if (!data) {
+        return {
+            notFound: true
+        }
+    }
+    return {
+        props: {
+            products: data
+        }
+    }
 }
 
 export default ProductsPage
