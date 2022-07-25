0acc74299c41ffb6738aae50d673f87a5b09f777
Tungduong2001 <tungduong.kaka64@gmail.com>
2022-07-25 09:42:34 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,6 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
 import { useAuth } from '../hooks/auth'
 import useProduct from '../hooks/use-product'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   const { data, error } = useAuth();
